f5fc28830a0f53d90b6c0c4b6af64d01d2423983
PrathameshPowar-dan <powarprathamesh579@gmail.com>
2026-02-20 23:40:06 +0530

    Multer and Cloudinary

--- controllers/creations/creationsController.ts
+++ controllers/creations/creationsController.ts
@@ -1,9 +1,35 @@
 import { Request, Response } from "express";
+import { prisma } from "../../configs/prisma.js";
 
 // Controller to create image
 export const createImageCreation = async (req: Request, res: Response) => {
+    let tempProjectId: string;
+    const { userId } = req.auth();
+    let isImageCreditDeducted = false;
+
+    const { name = "PRo1", aspectRatio, userPrompt, productName, targetLength = 5 } = req.body;
+
+    const images: any = req.files;
+
+    if (images.length < 2 || !productName) {
+        return res.status(400).json({ error: "At least 2 images and product name are required" });
+    }
+
+    const user = await prisma.user.findUnique({
+        where: { id: userId }
+    });
+
+    if (!user || user.ImageCredits < 1) {
+        return res.status(400).json({ error: "Insufficient image credits" });
+    } else {
+        await prisma.user.update({
+            where: { id: userId },
+            data: { ImageCredits: { decrement: 1 } }
+        }).then(() => { isImageCreditDeducted = true });
+    };
+
     try {
-        
+
     } catch (error: any) {
         console.error("Error creating image creation:", error);
         res.status(500).json({ error: "Failed to create image creation" });
@@ -13,7 +39,7 @@ export const createImageCreation = async (req: Request, res: Response) => {
 // Controller to create video
 export const createVideoCreation = async (req: Request, res: Response) => {
     try {
-        
+
     } catch (error: any) {
         console.error("Error creating video creation:", error);
         res.status(500).json({ error: "Failed to create video creation" });
@@ -23,7 +49,7 @@ export const createVideoCreation = async (req: Request, res: Response) => {
 // Controller to delete creation
 export const deleteCreation = async (req: Request, res: Response) => {
     try {
-        
+
     } catch (error: any) {
         console.error("Error deleting creation:", error);
         res.status(500).json({ error: "Failed to delete creation" });
--- routes/creationRoutes.ts
+++ routes/creationRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { protect } from '../middlewares/auth.js';
 import { createImageCreation, createVideoCreation, deleteCreation } from '../controllers/creations/creationsController.js';
+import upload from '../configs/multer.js';
 
 const router = express.Router();
 
-router.post('/image', protect, createImageCreation);
+router.post('/image',upload.array('images',2), protect, createImageCreation);
 router.post('/video', protect, createVideoCreation);
 router.delete('/delete/:id', protect, deleteCreation);
 
