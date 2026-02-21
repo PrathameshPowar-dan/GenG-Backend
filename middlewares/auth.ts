a694410601a83815a2899ee5912a695a3cdc3438
PrathameshPowar-dan <powarprathamesh579@gmail.com>
2026-02-21 21:54:53 +0530

    WORKED

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -2,7 +2,13 @@ import { Request, Response, NextFunction } from "express";
 
 export const protect = async (req: Request, res: Response, next: NextFunction) => {
     try {
+        // 1. Check if the token is even arriving
+        // console.log("Auth Header:", req.headers.authorization);
+        
+        // 2. Check what Clerk parsed
+        // console.log("Clerk Auth Object:", req.auth());
         const {userId} = req.auth();
+        console.log(userId)
 
         if (!userId) {
             return res.status(401).json({ message: "Unauthorized" });
@@ -10,6 +16,7 @@ export const protect = async (req: Request, res: Response, next: NextFunction) =
 
         next();
     } catch (error: any) {
+        console.error("Auth Middleware Error:", error);
         res.status(401).json({ message: error.message || "Internal server error" });
     }
 };
